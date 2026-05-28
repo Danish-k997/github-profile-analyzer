46d643c0aed59a7bc84e2fa5cc0acfe7b400a257
Danish--k997 <danishkha8321@gmail.com>
2026-05-28 13:54:50 +0530

    complete allprofile api

--- Server/Controller/analyzercontroller.js
+++ Server/Controller/analyzercontroller.js
@@ -16,6 +16,13 @@ export const SingleProfileAnalyze = async (req, res) => {
     const response = await fetch(Api + username);
     const data = await response.json();
 
+    if (!response.ok) {
+      return res.status(404).json({
+        success: false,
+        message: "GitHub user not found",
+      });
+    }
+
     const analyzedData = {
       githubId: data.id,
       username: data.login,
@@ -27,17 +34,85 @@ export const SingleProfileAnalyze = async (req, res) => {
       following: data.following,
       location: data.location,
       company: data.company,
+       profileUrl: data.html_url,
     };
 
     const profile = await prisma.githubProfile.upsert({
       where: {
         username: analyzedData.username,
       },
+
+      update: analyzedData,
+
+      create: analyzedData,
     });
 
-    res.json(profile);
+    return res.status(200).json({
+      success: true,
+      data: profile,
+    });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+
+export const getAllProfiles = async (req, res) => {
+  try {
+   
+    const page = Math.max(parseInt(req.query.page) || 1, 1);
+
+    const limit = Math.min(
+      Math.max(parseInt(req.query.limit) || 10, 1),
+      50
+    );
+
+    const skip = (page - 1) * limit;
+
+    
+    const [profiles, totalProfiles] = await Promise.all([
+      prisma.githubProfile.findMany({
+        skip,
+        take: limit,
+
+        select: {
+          id: true,
+          username: true,
+          avatarUrl: true,
+          followers: true,
+          following: true,
+          publicRepos: true,
+          createdAt: true,
+        },
+
+        orderBy: {
+          createdAt: "desc",
+        },
+      }),
+
+      prisma.githubProfile.count(),
+    ]);
+
+    return res.status(200).json({
+      success: true,
+
+      pagination: {
+        currentPage: page,
+        totalPages: Math.ceil(totalProfiles / limit),
+        totalProfiles,
+        limit,
+      },
+
+      data: profiles,
+    });
+
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
   }
 };
--- Server/router/analyzerrouter.js
+++ Server/router/analyzerrouter.js
@@ -4,5 +4,6 @@ import * as analyzer  from '../Controller/analyzercontroller.js';
 const router = express.Router();
 
 router.post('/getprofile', analyzer.SingleProfileAnalyze);
+router.get('/getallprofiles', analyzer.getAllProfiles);
 
 export default router;
